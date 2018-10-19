38675e308aecf7559cdb77695000e7f8af4d3eb1
Dalton Lima <daltonvarussa@gmail.com>
2018-10-19 12:03:03 -0300

    add Voice and some new animations

--- Assets/Scripts/Panda.cs
+++ Assets/Scripts/Panda.cs
@@ -6,19 +6,23 @@ using UnityEngine.Assertions;
 //using HoloToolkit.Unity.Speech;
 
 [RequireComponent(typeof(Animator))]
-public class Panda : MonoBehaviour, IInputHandler/*, ISpeechHandler*/
+public class Panda : MonoBehaviour, IInputHandler, IFocusable/*, ISpeechHandler*/
 {
     private Animator _animator;
     private Vector3 _originalPosition;
     [Tooltip("The GameObject that the Panda will eat")]
     [SerializeField] private GameObject _bamboo;
     [Tooltip("The Panda Paw to hold the food/bamboo")]
-    [SerializeField] private Transform _pawToHoldBamboo;  // 'PANDA/new_Bone031' in the model
+    [SerializeField] private Transform _pawToHoldBamboo;  // 'PANDA/new_Bone009' in the model is the left paw
 
     // Animator Hashes, for efficiency
     private int _isEatingParameterHash = Animator.StringToHash("IsEating");
     private int _isLayingDownParameterHash = Animator.StringToHash("IsLayingDown");
     private int _isIdlingParameterHas = Animator.StringToHash("IsIdling");
+
+    private bool _isEating = false;
+    private bool _isLayingDown = false;
+    private bool _isIdling = false;
 
     void Awake()
     {
@@ -28,47 +32,88 @@ public class Panda : MonoBehaviour, IInputHandler/*, ISpeechHandler*/
         Assert.IsNotNull(_pawToHoldBamboo);
     }
 
+    void Start()
+    {
+        _originalPosition = this.transform.position;
+        SetIdle();
+    }
+
+    private void GrabBamboo()
+    {
+        _bamboo.transform.SetParent(_pawToHoldBamboo);
+        _bamboo.transform.localPosition = Vector3.zero;
+    }
+
     void IInputHandler.OnInputDown(InputEventData eventData)
     {
-        Debug.Log("OnInputDown");        
-        throw new System.NotImplementedException();
+        Debug.Log("OnInputDown");                
     }
 
     void IInputHandler.OnInputUp(InputEventData eventData)
     {
         Debug.Log("OnInputUp");
-        SetEat();
-        throw new System.NotImplementedException();
+
+        if (_isIdling)
+        {
+            SetEat();
+            GrabBamboo();
+            return;
+        }
+
+        if (_isEating)
+        {
+            SetLayDown();
+            return;
+        }
+        
+        if (_isLayingDown)
+        {
+            SetIdle();
+            return;
+        }
     }
 
-    void Start ()
+    void IFocusable.OnFocusEnter()
     {
-        _originalPosition = this.transform.position;
-        _bamboo.transform.SetParent(_pawToHoldBamboo);
-        _bamboo.transform.localPosition = Vector3.zero;
-	}
+        Debug.Log("OnFocusEnter");
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+        Debug.Log("OnFocusExit");
+    }
+
 
     #region Helper Methods
-    private void SetEat()
+    public void SetEat()
     {
-        _animator.SetBool(_isEatingParameterHash, true);
-        _animator.SetBool(_isIdlingParameterHas, false);
-        _animator.SetBool(_isLayingDownParameterHash, false);
+        _isEating = true;
+        _isIdling = false;
+        _isLayingDown = false;
+        SetAnimatorParameters();
     }
 
-    private void SetIdle()
+    public void SetIdle()
     {
-        _animator.SetBool(_isEatingParameterHash, false);
-        _animator.SetBool(_isIdlingParameterHas, true);
-        _animator.SetBool(_isLayingDownParameterHash, false);
+        _isEating = false;
+        _isIdling = true;
+        _isLayingDown = false;
+        SetAnimatorParameters();
     }
 
-    private void SetLayDown()
+    public void SetLayDown()
     {
-        _animator.SetBool(_isEatingParameterHash, false);
-        _animator.SetBool(_isIdlingParameterHas, false);
-        _animator.SetBool(_isLayingDownParameterHash, true);
+        _isEating = false;
+        _isIdling = false;
+        _isLayingDown = true;
+        SetAnimatorParameters();
     }
 
+    private void SetAnimatorParameters()
+    {
+        _animator.SetBool(_isEatingParameterHash, _isEating);
+        _animator.SetBool(_isIdlingParameterHas, _isIdling);
+        _animator.SetBool(_isLayingDownParameterHash, _isLayingDown);
+    }
     #endregion
 }
