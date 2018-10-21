7dedda80a204bd6e5d5e49988c18615177f4f485
Dalton Lima <daltonvarussa@gmail.com>
2018-10-21 19:06:09 -0300

    add sounds

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,166 @@
+﻿using HoloToolkit.Unity;
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MainManager : MonoBehaviour
+{
+    
+    private bool m_isInitialized;
+    [SerializeField]
+    private float kMinAreaForComplete = 20.0f;
+    [SerializeField]
+    private float kMinHorizontalAreaForComplete = 10.0f;
+    [SerializeField]
+    private float kMinWallAreaForComplete = 5.0f;
+
+    [SerializeField] private Panda _pandaPrefab;
+    [SerializeField] private GameObject _bambooPrefab;    
+
+    private Panda _activePanda;    
+
+    private Vector3 _floorPosition = Vector3.zero;
+    private HeadsUpDirectionIndicator _headsUpIndicator;
+
+    private SpatialUnderstandingDllTopology.TopologyResult[] resultsTopology = new SpatialUnderstandingDllTopology.TopologyResult[512];
+
+    void Awake()
+    {
+        Assert.IsNotNull(_pandaPrefab);
+        Assert.IsNotNull(_bambooPrefab);
+    }
+
+    void Start()
+    {
+        _headsUpIndicator = GameObject.FindObjectOfType<HeadsUpDirectionIndicator>();        
+        SpatialUnderstanding.Instance.ScanStateChanged += Instance_ScanStateChanged;        
+    }
+
+    //private void OnTappedEvent(TappedEventArgs tappedEventArgs)
+    //{
+    //    if (_villain == null) { return; }
+    //    _villain.KillVictim();
+    //    DestroyNPCs();
+    //    if (_floorPosition != Vector3.zero) { InstantiateNPCs(_floorPosition); }
+    //}
+
+    private void Instance_ScanStateChanged()
+    {
+        if ((SpatialUnderstanding.Instance.ScanState == SpatialUnderstanding.ScanStates.Done) &&
+    SpatialUnderstanding.Instance.AllowSpatialUnderstanding)
+        {
+            FinishSetup();
+        }
+    }
+
+    void Update()
+    {
+        // check if enough of the room is scanned
+        if (!m_isInitialized && DoesScanMeetMinBarForCompletion)
+        {
+            // let service know we're done scanning
+            SpatialUnderstanding.Instance.RequestFinishScan();
+            m_isInitialized = true;
+        }
+    }
+
+    public bool DoesScanMeetMinBarForCompletion
+    {
+        get
+        {
+            // Only allow this when we are actually scanning
+            if ((SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Scanning) ||
+                (!SpatialUnderstanding.Instance.AllowSpatialUnderstanding))
+            {
+                return false;
+            }
+
+            // Query the current playspace stats
+            IntPtr statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStatsPtr();
+            if (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(statsPtr) == 0)
+            {
+                return false;
+            }
+            SpatialUnderstandingDll.Imports.PlayspaceStats stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+
+            // Check our preset requirements
+            if ((stats.TotalSurfaceArea > kMinAreaForComplete) ||
+                (stats.HorizSurfaceArea > kMinHorizontalAreaForComplete) ||
+                (stats.WallSurfaceArea > kMinWallAreaForComplete))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void FinishSetup()
+    {
+        // use spatial understanding to find floor
+        SpatialUnderstandingDll.Imports.QueryPlayspaceAlignment(SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceAlignmentPtr());
+        SpatialUnderstandingDll.Imports.PlayspaceAlignment alignment = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceAlignment();
+
+        // find large floor area
+        IntPtr resultsTopologyPtr = SpatialUnderstanding.Instance.UnderstandingDLL.PinObject(resultsTopology);
+        int locationCount = SpatialUnderstandingDllTopology.QueryTopology_FindLargestPositionsOnFloor(
+            resultsTopology.Length, resultsTopologyPtr);
+
+        // hide mesh
+        var customMesh = SpatialUnderstanding.Instance.GetComponent<SpatialUnderstandingCustomMesh>();
+        customMesh.DrawProcessedMesh = false;
+
+        // find some topology data
+        Vector3 floorPosition;
+
+        if (locationCount > 0)
+        {
+            // retrieve a large floor area from spatial understanding
+            floorPosition = resultsTopology[0].position;
+            _floorPosition = floorPosition;
+        }
+        else
+        {
+            // just get floor in front of the player
+            var inFrontOfCamera = Camera.main.transform.position + Camera.main.transform.forward * 2.0f;
+            floorPosition = new Vector3(inFrontOfCamera.x, alignment.FloorYValue, 2.69f);
+        }
+        //if (_floorObject != null)
+        {            
+            InstantiateNPCs(floorPosition);
+        }
+    }
+
+    private void InstantiateNPCs(Vector3 floorPosition)
+    {        
+        if (_pandaPrefab != null)
+        {            
+            _activePanda = Instantiate(_pandaPrefab.gameObject, floorPosition, Quaternion.identity).GetComponent<Panda>();
+
+            if (_headsUpIndicator != null)
+            {
+                _headsUpIndicator.gameObject.SetActive(true);
+                Debug.Log("HUD Indicator active");
+                _headsUpIndicator.TargetObject = _activePanda.gameObject;
+            }
+        }
+    }
+
+    private void DestroyNPCs()
+    {
+        if (_activePanda.gameObject != null)
+        {
+            Destroy(_activePanda.gameObject);
+        }        
+    }
+
+    private void HideNPCs()
+    {        
+        if (_activePanda) DestroyImmediate(_activePanda);        
+    }
+
+    public void OnReset()
+    {
+        HideNPCs();
+        InstantiateNPCs(_floorPosition);
+    }
+}
--- Assets/Scripts/Panda.cs
+++ Assets/Scripts/Panda.cs
@@ -6,14 +6,16 @@ using UnityEngine.Assertions;
 //using HoloToolkit.Unity.Speech;
 
 [RequireComponent(typeof(Animator))]
-public class Panda : MonoBehaviour, IInputHandler, IFocusable/*, ISpeechHandler*/
+public class Panda : MonoBehaviour, IInputHandler/*, IFocusable, ISpeechHandler*/
 {
     private Animator _animator;
     private Vector3 _originalPosition;
+    private Vector3 _bambooInitialPosition;
     [Tooltip("The GameObject that the Panda will eat")]
     [SerializeField] private GameObject _bamboo;
     [Tooltip("The Panda Paw to hold the food/bamboo")]
     [SerializeField] private Transform _pawToHoldBamboo;  // 'PANDA/new_Bone009' in the model is the left paw
+    [SerializeField] private AudioSource _audioSource;
 
     // Animator Hashes, for efficiency
     private int _isEatingParameterHash = Animator.StringToHash("IsEating");
@@ -24,39 +26,59 @@ public class Panda : MonoBehaviour, IInputHandler, IFocusable/*, ISpeechHandler*
     private bool _isLayingDown = false;
     private bool _isIdling = false;
 
+    [SerializeField] private AudioClip _bearBreath;
+    [SerializeField] private AudioClip _bearSounds;
+    [SerializeField] private AudioClip _chewing;
+
     void Awake()
     {
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
         Assert.IsNotNull(_animator);
         Assert.IsNotNull(_bamboo);
         Assert.IsNotNull(_pawToHoldBamboo);
+        Assert.IsNotNull(_audioSource);
+        Assert.IsNotNull(_bearBreath);
+        Assert.IsNotNull(_bearSounds);
+        Assert.IsNotNull(_chewing);
     }
 
     void Start()
     {
+        _bambooInitialPosition = _bamboo.transform.position;
         _originalPosition = this.transform.position;
         SetIdle();
     }
 
-    private void GrabBamboo()
+    public void SetBamboo(GameObject bamboo)
     {
-        _bamboo.transform.SetParent(_pawToHoldBamboo);
-        _bamboo.transform.localPosition = Vector3.zero;
+        if (_bamboo != null)
+        {
+            _bamboo = bamboo;
+        }
+    }
+
+    private void ShowBamboo()
+    {
+        _bamboo.gameObject.SetActive(true);
+    }
+
+    private void HideBamboo()
+    {
+        _bamboo.gameObject.SetActive(false);
     }
 
     void IInputHandler.OnInputDown(InputEventData eventData)
     {
-        Debug.Log("OnInputDown");                
+        //Debug.Log("OnInputDown");                
     }
 
     void IInputHandler.OnInputUp(InputEventData eventData)
     {
-        Debug.Log("OnInputUp");
-
+        //Debug.Log("OnInputUp");
         if (_isIdling)
         {
-            SetEat();
-            GrabBamboo();
+            SetEat();            
             return;
         }
 
@@ -73,40 +95,43 @@ public class Panda : MonoBehaviour, IInputHandler, IFocusable/*, ISpeechHandler*
         }
     }
 
-    void IFocusable.OnFocusEnter()
-    {
-        Debug.Log("OnFocusEnter");
-    }
-
-    void IFocusable.OnFocusExit()
-    {
-        Debug.Log("OnFocusExit");
-    }
-
-
     #region Helper Methods
     public void SetEat()
     {
         _isEating = true;
         _isIdling = false;
         _isLayingDown = false;
-        SetAnimatorParameters();
+        SetAnimatorParameters();        
+        StartCoroutine(ShowAndHideBamboo());
+        StartCoroutine(PlayClip(_chewing, 6f));
+    }
+
+    IEnumerator ShowAndHideBamboo()
+    {
+        _bamboo.gameObject.SetActive(true);
+        float animLength = 9.5f;
+        yield return new WaitForSeconds(animLength);
+        _bamboo.gameObject.SetActive(false);
     }
 
     public void SetIdle()
     {
+        HideBamboo();
         _isEating = false;
         _isIdling = true;
         _isLayingDown = false;
         SetAnimatorParameters();
+        StartCoroutine(PlayClip(_bearSounds));
     }
 
     public void SetLayDown()
     {
+        HideBamboo();
         _isEating = false;
         _isIdling = false;
         _isLayingDown = true;
         SetAnimatorParameters();
+        StartCoroutine(PlayClip(_bearBreath));
     }
 
     private void SetAnimatorParameters()
@@ -114,6 +139,16 @@ public class Panda : MonoBehaviour, IInputHandler, IFocusable/*, ISpeechHandler*
         _animator.SetBool(_isEatingParameterHash, _isEating);
         _animator.SetBool(_isIdlingParameterHas, _isIdling);
         _animator.SetBool(_isLayingDownParameterHash, _isLayingDown);
+    }
+
+    IEnumerator PlayClip(AudioClip clip, float delayToStartInSeconds = 0f)
+    {
+        if (clip == null) yield break;
+
+        _audioSource.Stop();
+        yield return new WaitForSeconds(delayToStartInSeconds);
+        _audioSource.clip = clip;
+        _audioSource.Play();
     }
     #endregion
 }
